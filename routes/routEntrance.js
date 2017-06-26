566a53212b16fb8a827d831a6483135026221fa0
wu199406 <1070471160@qq.com>
2017-06-26 21:42:23 +0800

    1:修改路由入口文件；

--- routes/routEntrance.js
+++ routes/routEntrance.js
@@ -1,5 +1,6 @@
 /**
  * 用于配置路由入口
+ * 所有的路由模块都该处导入,通过在app.js文件中引用该模块并调用路由配置方法routConfigFun配置路由
  * Created by wu199406 on 2017/6/26.
  */
 
@@ -13,9 +14,11 @@ let resource = require("../routes/resourceRout");
  * 将路由配置到app
  * @param app
  */
-module.exports = function(app){
+let routConfigFun = function(app){
     app.use('/', index);
     app.use('/users', users);
     app.use('/role', role);
     app.use("/resource",resource);
 };
+
+module.exports = routConfigFun;
