1bfbfb7c6a0894dd3c1829bcff442b807b87f4fc
wu199406 <1070471160@qq.com>
2018-01-17 17:49:08 +0800

    1:添加后台管理首页; 2:修改资源编辑窗口中的父资源的combotree的数据,使得资源能成为顶层资源;
    Signed-off-by: wu199406 <1070471160@qq.com>

--- routes/indexRout.js
+++ routes/indexRout.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('client/index', { title: '首页' });
 });
 
+router.get('/index', function(req, res, next) {
+    res.render('index', { title: '后台管理系统首页' });
+});
+
 module.exports = router;
